f8dfdd17845c6363ade0fc2e30c6c019bea4185e
Okonkwo Kamso <okonkwochikamsoj@gmail.com>
2023-10-31 15:18:02 +0100

    Adding all to commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+//1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.NODE_ENV);
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
+
+
